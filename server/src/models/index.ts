34da866cb9f406f73bee73292c37384730a3f9e8
JiliJiang <jiangjili@gmail.com>
2024-11-19 20:50:26 -0800

    change

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,10 +6,19 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: 'postgres',
+      ssl: true,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+  }}})
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
+      ssl: true,
+    
       dialectOptions: {
         decimalNumbers: true,
       },
